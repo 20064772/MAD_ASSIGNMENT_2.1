b74e5b3579e5334a1706587f92af1218260cf97c
ryanmckenney <20064772@student.curtin.edu.au>
2022-10-11 08:59:29 +0800

    working on the recyc view

--- app/src/main/java/com/example/mad_assignment_21/UserViewHolder.java
+++ app/src/main/java/com/example/mad_assignment_21/UserViewHolder.java
@@ -1,6 +1,13 @@
 package com.example.mad_assignment_21;
 
-public class UserViewHolder {
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class UserViewHolder extends RecyclerView.ViewHolder {
 
 
+    public UserViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
 }
